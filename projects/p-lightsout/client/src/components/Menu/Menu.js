372d4ff6516d5c58fdfe9031651831453b02af81
larsm2v2 <larsm2v2@users.noreply.github.com>
2024-03-07 10:24:20 -0500

    Sectioned for my projects only

---
+++ projects/p-lightsout/client/src/components/Menu/Menu.js
@@ -0,0 +1,74 @@
+import React, {Fragment} from "react";
+
+const Menu = () => {
+
+    /*{" "}*/
+    <Fragment>
+        
+        <table className="table mt-5 text-center">
+    <thead>
+        <tr>
+            <th>End Game</th>
+            <td>
+                <button
+                className="btn btn-warning"
+                //onClick={() => }
+                >End Game
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <th>Start</th>
+            <td>
+                <button
+                className="btn btn-warning"
+                //onClick={() => }
+                >
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <th>Sound</th>
+            <td>
+                <button
+                className="btn btn-warning"
+                //onClick={() => }
+                >
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <th>Help</th>
+            <td>
+                <button
+                className="btn btn-warning"
+                //onClick={() => }
+                >
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <th>Modes</th>
+            <td>
+                <button
+                className="btn btn-warning"
+                //onClick={() => }
+                >
+                </button>
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+        {/*<tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+        */}
+    </tbody>
+  </table>
+    </Fragment>
+};
+
+export default Menu
+
